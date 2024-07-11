0c62e0a487dfdc4fabbe0481cd55eaa8ae549b3a
Lautaro Santillan <santillanlautaro03@gmail.com>
2024-07-11 17:06:20 -0300

    Day 3

---
+++ 003-NumeroPrimo.js
@@ -0,0 +1,24 @@
+/*
+ * Escribe un programa que se encargue de comprobar si un número es o no primo.
+ * Hecho esto, imprime los números primos entre 1 y 100.
+ */
+function esPrimo(numero) {
+    if (numero <= 1) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(numero); i++) {
+        if (numero % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Imprimir los números primos entre 1 y 100
+for (let i = 1; i <= 100; i++) {
+    if (esPrimo(i)) {
+        console.log(i);
+    }
+}
+
+/* 11/7/2024 */
